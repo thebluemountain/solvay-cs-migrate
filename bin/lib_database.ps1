dd35cd61fbe0bfab9201821081af8de01f6e2d50
thevef <frederic.thevenet@emc.com>
2014-10-23 11:19:56 +0200

    - Refactoring (split the main script into several sub-scripts) - Create registry entries for the docbase - Create a Windows service for the docbase

---
+++ bin/lib_database.ps1
@@ -0,0 +1,40 @@
+﻿
+<# ODBC/SQL related methods #>
+<#
+ the function that returns an opened connection to the database
+ @dsn: identifies an ODBC data source's name
+ @user: holds the name of the user to connect as
+ @pwd: holds the password to use
+ #>
+function getConnect ($dsn, $user, $pwd)
+{
+ $cnx = new-object System.Data.Odbc.OdbcConnection
+ $cnx.ConnectionString = 'dsn=' + $dsn + ';uid=' + $user + ';pwd=' + $pwd
+ $cnx.Open()
+ if ($cnx.State -ne [System.Data.ConnectionState]::Open)
+ {
+  throw 'can''t open database ' + $dsn
+ }
+ return $cnx
+}
+
+<#
+ the method that retrieves results matching the supplied SQL (select) query
+ @returns: the first returned (data)table
+ #>
+function selectTable ($cnx, $sql)
+{
+ $da = new-object System.Data.Odbc.OdbcDataAdapter $sql,$cnx
+ try
+ {
+  $table = new-object System.Data.DataTable
+  $da.Fill($table) | out-null
+  # be carefull: it's considered as a collection ...
+  # therefore returned as an array is not empty
+  return ,$table
+ }
+ finally
+ {
+  $da.Dispose()
+ }
+}
---
+++ bin/lib_docbase_reg.ps1
@@ -0,0 +1,59 @@
+﻿function Write-DocbaseRegKey( $obj)
+{
+    if ($null -eq $obj)
+    {
+        throw "Argument obj cannot be null"
+    }
+
+    $dmp = _DumpObjAt $obj 
+    Write-Verbose "Reg object=$dmp"
+
+    if (test-path $obj.Path)
+    {
+        throw "The registry key $($obj.Path) already exists"
+    }
+
+    $out = New-Item -Path $obj.Path -type directory -force
+    Write-Output "Reg key $out successfully created"
+    foreach ($name in $obj.Keys)
+    {
+        $value = $obj.($name)
+        $out = New-ItemProperty -Path $obj.Path -Name $name -PropertyType String -Value $value 
+        Write-Output "Reg entry $name = $value successfully created"
+    }    
+}
+
+function New-DocbaseService($obj)
+{
+    if ($null -eq $obj)
+    {
+       throw "Argument obj cannot be null"
+    }
+
+    $dmp = _DumpObjAt $obj
+    Write-Verbose "Svc object dump=$dmp"
+   
+    if (Test-DocbaseService $obj.name)
+    {
+        throw "The docbase service $($obj.name) already exists"
+    }   
+    $out = New-Service -Name $obj.name -DisplayName $obj.display -StartupType Automatic -BinaryPathName $obj.commandLine -Credential $obj.credentials
+    Write-Verbose $out
+    Write-Output "Docbase service $($obj.name) successfully created."
+}
+
+function Test-DocbaseService($name)
+{
+    if ($null -eq $name)
+    {
+        throw "Argument obj cannot be null"
+    }
+    $out = Get-Service -Name $name -ErrorAction SilentlyContinue -ErrorVariable svcErr
+    if ($svcErr)
+    {
+        return $false
+    }
+    return $true
+   
+}
+
